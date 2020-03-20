9fed0727945019c7eb81108d0cda613e7ed0b6c4
Wesley Willians <wesleywillians@gmail.com>
2020-03-19 20:54:50 -0400

    github actions

--- driver.go
+++ driver.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"os"
 
 	"github.com/gorilla/mux"
 )
+
+type Driver struct {
+	Uuid string `json:"uuid"`
+	Name string `json:"name"`
+}
+
+type Drivers struct {
+	Drivers []Driver
+}
 
 func loadDrivers() []byte {
 
@@ -29,8 +39,24 @@ func ListDrivers(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(drivers))
 }
 
+func GetDriverById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	data := loadDrivers()
+
+	var drivers Drivers
+	json.Unmarshal(data, &drivers)
+
+	for _, v := range drivers.Drivers {
+		if v.Uuid == vars["id"] {
+			driver, _ := json.Marshal(v)
+			w.Write([]byte(driver))
+		}
+	}
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/drivers", ListDrivers)
+	r.HandleFunc("/drivers/{id}", GetDriverById)
 	http.ListenAndServe(":8081", r)
 }
